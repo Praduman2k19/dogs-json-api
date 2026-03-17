91ed6b2f4edf8724370f38d8a28eea29fec1885f
Praduman Kumar <praduman2k19@gmail.com>
2026-03-18 00:47:55 +0530

    added testing api

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(cors());
 
 const dogRoutes = require("./routes/dogRoutes");
 app.use("/api/dogs", dogRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Server working 🚀");
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
